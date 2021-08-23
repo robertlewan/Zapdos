c60c2ec2c921c667cfc41630ec2c4d577ce0b35f
robertlewan <anhtri.ngo99@gmail.com>
2021-08-23 04:01:06 +0000

    fix embed and complete list of pokemon

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ my_token = os.environ['my_token']
 client = commands.Bot(command_prefix='?', case_insensity=True)
 
 def get_pkm_info(pkm):
-    f = open('data.json', )
-    data = json.load(f)
-    for i in data['pkm_details']:
-        if i['name'] == pkm:
-            pkm_info = i
-            return pkm_info
-
+	f = open('data.json', )
+	data = json.load(f)
+	for i in data['pkm_details']:
+		if i['name'] == pkm:
+			pkm_info = i
+			return pkm_info
+			print(pkm_info)
+	f.close()
 
 @client.event
 async def on_ready():
@@ -32,31 +33,32 @@ async def on_message(message):
 	if message.content.startswith('?pkm'):
 		pkm = message.content.lower()[5:]
 		pkm_info = get_pkm_info(pkm)
+		print(pkm)
 
-		embed = discord.Embed(title=pkm_info["name"], colour=discord.Colour(0xffffff),description="```\n```")
+		embed = discord.Embed(title=pkm_info["name"].capitalize(), colour=discord.Colour(0xffffff))
 
-		embed.set_image(url="https://www.serebii.net/pokemonunite/pokemon/003.png")
+		embed.set_image(url=pkm_info["sprite"])
 		embed.set_footer(text="All stats taken at level 15")
 
-		embed.add_field(name="Evolution", value="lvl 5 => lvl 9", inline=True)
-		embed.add_field(name="Style", value="Ranged", inline=True)
-		embed.add_field(name="Attack damage", value="Special", inline=True)
-		embed.add_field(name="Role", value="Attacker", inline=True)
-		embed.add_field(name="Price", value="6000 coins", inline=True)
-		embed.add_field(name="Ability", value="Overgrowth: When the Pokémon is at low HP, the damage it deals is increased.")
-		embed.add_field(name="Basic attack", value="Physical", inline=True)
-		embed.add_field(name="Boosted", value="Special", inline=True)
-		embed.add_field(name="Move 1", value="Seed Bomb, upgrade at level 5 to Sludge Bomb or Giga Drain.")
-		embed.add_field(name="Move 2", value="Razor Leaf, upgrade at level 7 to Solar Beam or Petal Dance.")
-		embed.add_field(name="Unite move", value="Verdant Anger")
-		embed.add_field(name="Speed", value="2564", inline=True)
-		embed.add_field(name="HP", value="5235", inline=True)
-		embed.add_field(name="Attack", value="5235", inline=True)
-		embed.add_field(name="Defense", value="5235", inline=True)
-		embed.add_field(name="Sp. Attack", value="5235", inline=True)
-		embed.add_field(name="Sp. Defense", value="5235", inline=True)
+		embed.add_field(name="Evolution", value=pkm_info["evolution"], inline=True)
+		embed.add_field(name="Style", value=pkm_info["style"], inline=True)
+		embed.add_field(name="Attack damage", value=pkm_info["attack-dmg"], inline=True)
+		embed.add_field(name="Role", value=pkm_info["role"], inline=True)
+		embed.add_field(name="Price", value=pkm_info["price"], inline=True)
+		embed.add_field(name="Ability", value=pkm_info["ability"], inline=False)
+		embed.add_field(name="Basic attack", value=pkm_info["basic"], inline=True)
+		embed.add_field(name="Boosted", value=pkm_info["boosted"], inline=True)
+		embed.add_field(name="Move 1", value=pkm_info["move1"] + ', upgrades at level ' + pkm_info['move1-upg-lvl'] + ' to ' + pkm_info['move1.1'] + ' or ' + pkm_info['move1.2'], inline=False)
+		embed.add_field(name="Move 2", value=pkm_info["move2"] + ', upgrades at level ' + pkm_info['move2-upg-lvl'] + ' to ' + pkm_info['move2.1'] + ' or ' + pkm_info['move2.2'], inline=False)
+		embed.add_field(name="Unite move", value=pkm_info["unite"], inline=False)
+		embed.add_field(name="Speed", value=pkm_info["speed"], inline=True)
+		embed.add_field(name="HP", value=pkm_info["hp"], inline=True)
+		embed.add_field(name="Attack", value=pkm_info["atk"], inline=True)
+		embed.add_field(name="Defense", value=pkm_info["def"], inline=True)
+		embed.add_field(name="Sp. Attack", value=pkm_info["sp-atk"], inline=True)
+		embed.add_field(name="Sp. Defense", value=pkm_info["sp-def"], inline=True)
 
-		await client.say(embed=embed)
+		await message.channel.send(embed=embed)
 
 
 # print(get_pkm_info('venusaur'))
